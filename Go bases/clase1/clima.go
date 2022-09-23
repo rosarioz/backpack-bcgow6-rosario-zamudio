40af32a5f4901d4894ffa6e26700a51ef952df3f
rzamudio <rosario.zamudio@mercadolibre.com>
2022-09-23 13:01:50 -0300

    clase go bases

---
+++ Go bases/clase1/clima.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	var(
+		humedad = 17
+		temperatura = 21
+		presion = 1.015
+	)
+
+	fmt.Print("la humedad es ",  humedad , ", la temperatura es ", temperatura, ", la presion es ", presion)
+
+
+
+	//var nombre string
+  	//var apellido int
+  	//var int edad
+  	//apellido := 6
+  	//var licenciaDeConducir = true
+  	//var estaturaDeLaPersona int
+  	//var cantidadDeHijos = 2
+
+	//var apellido string = "Gomez"
+  	//var edad int = 35
+  	//boolean := false
+  	//var sueldo string = "45857.90"
+  	//var nombre string = "Julián"
+
+
+
+
+}
---
+++ Go bases/clase1/nombre.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	nombre := "Rosario Zamudio"
+	direccion := "La Pampa 2532 Oeste, San Juan"
+
+	fmt.Println("mi nombre es: " + nombre + " y vivo en: " + direccion)
+}
